917a97c1479134fc1b8d6662d3b214ccfae83ab4
yyxg <441887221@qq.com>
2022-06-03 08:57:12 +0800

    Initial commit

---
+++ src/main/java/com/example/common_demo/CommonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.common_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/common_demo/CommonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
